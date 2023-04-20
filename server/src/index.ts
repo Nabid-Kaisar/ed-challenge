baa4775da8c86aedecb206c7be20a6e1e4b5bd91
Nabid Kaisar <himelkaisar023@gmail.com>
2023-04-20 15:05:12 +0600

    DUMMY Response sent from server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from 'express';
 import {SERVER_PORT} from "../../constants/CONSTANTS";
+import {DUMMY_RESPONSE} from "../../constants/DUMMY_RESPONSE";
 
 const app = express();
 const port: number = SERVER_PORT;
 
 app.get('/api/promotions/priceoffers/ond/:origin/:destination', (req: Request, res: Response) => {
-    res.json({msg: 'Hello, World!'});
+    res.json(DUMMY_RESPONSE);
 });
 
 app.listen(port, () => {
