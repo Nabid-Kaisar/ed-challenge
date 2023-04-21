111e8aa406e9e9d8dfbbda445d0530f700630435
Nabid Kaisar <himelkaisar023@gmail.com>
2023-04-21 16:44:40 +0600

    Submit Button disabled while submitting form

--- client/src/forms/FlightSelectionForm.tsx
+++ client/src/forms/FlightSelectionForm.tsx
@@ -1,68 +1,62 @@
-import React, { ChangeEvent, useState } from "react";
-import { IATA } from "../models/IATAType";
-import Input from "../components/common/Input";
+import React, { ChangeEvent, useState } from 'react'
+import { IATA } from '../models/IATAType'
+import Input from '../components/common/Input'
 
 const FlightSelectionForm = ({
-  handleCallPromotionPricesApi,
-  origin,
-  setOrigin,
-  destination,
-  setDestination,
+    handleCallPromotionPricesApi,
+    origin,
+    setOrigin,
+    destination,
+    setDestination,
 }: FlightSelectionFormProps) => {
-  // const [origin, setOrigin] = useState("");
-  // const [destination, setDestination] = useState("");
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        setIsSubmitting(true)
+        handleCallPromotionPricesApi(origin as IATA, destination as IATA)
+            .then((res) => setIsSubmitting(false))
+            .catch((err) => setIsSubmitting(false))
+    }
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Origin:", origin);
-    console.log("Destination:", destination);
-    handleCallPromotionPricesApi(origin as IATA, destination as IATA);
-  };
-
-  return (
-    <div className="form-container">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <Input
-            label={"Origin"}
-            type="text"
-            id="origin"
-            name="origin"
-            value={origin}
-            onChange={(event: ChangeEvent<HTMLInputElement>) =>
-              setOrigin(event.target.value)
-            }
-            required
-          />
+    return (
+        <div className="form-container">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <Input
+                        label={'Origin'}
+                        type="text"
+                        id="origin"
+                        name="origin"
+                        value={origin}
+                        onChange={(event: ChangeEvent<HTMLInputElement>) => setOrigin(event.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <Input
+                        label={'Destination'}
+                        type="text"
+                        id="destination"
+                        name="destination"
+                        value={destination}
+                        onChange={(event: any) => setDestination(event.target.value)}
+                        required
+                    />
+                </div>
+                <button disabled={isSubmitting} className="submit-button" type="submit">
+                    Search Flights
+                </button>
+            </form>
         </div>
-        <div>
-          <Input
-            label={"Destination"}
-            type="text"
-            id="destination"
-            name="destination"
-            value={destination}
-            onChange={(event: any) => setDestination(event.target.value)}
-            required
-          />
-        </div>
-        <button className="submit-button" type="submit">
-          Search Flights
-        </button>
-      </form>
-    </div>
-  );
-};
+    )
+}
 
-export default FlightSelectionForm;
+export default FlightSelectionForm
 
 interface FlightSelectionFormProps {
-  handleCallPromotionPricesApi: (
-    origin: IATA,
+    handleCallPromotionPricesApi: (origin: IATA, destination: IATA) => Promise<void>
+    origin: IATA
+    setOrigin: (value: ((prevState: string) => string) | string) => void
     destination: IATA
-  ) => Promise<void>;
-  origin: IATA;
-  setOrigin: (value: ((prevState: string) => string) | string) => void;
-  destination: IATA;
-  setDestination: (value: ((prevState: string) => string) | string) => void;
+    setDestination: (value: ((prevState: string) => string) | string) => void
 }
